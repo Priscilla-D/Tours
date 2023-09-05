420ca0dff20b57361207f8d2abc45b246de32ab0
Priscilla-D <durand.priscilla@outlook.fr>
2023-09-05 11:19:49 +0200

    add fetching datas

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./App.css";
 
+const url = "https://course-api.com/react-tours-project";
+
 function App() {
+  const [tours, setTours] = useState([]);
+
+  const fetchDatas = async () => {
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      if (!response.ok) {
+        throw response;
+      }
+      setTours(tours);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchDatas();
+  }, []);
+
   return <></>;
 }
 
